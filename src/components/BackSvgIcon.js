1da604a044ec607bd8537d2ea528c1f3a5528a44
Piotr Sitarz <piotrsitarz@onet.eu>
2018-10-14 17:29:33 +0200

    adding contact page

---
+++ src/components/BackSvgIcon.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const BackSvgIcon = () => (
+  <div>
+    <svg viewBox="0 0 459 459">
+      <g>
+        <g>
+          <path d="M178.5,140.25v-102L0,216.75l178.5,178.5V290.7c127.5,0,216.75,40.8,280.5,130.05C433.5,293.25,357,165.75,178.5,140.25z"></path>
+        </g>
+      </g>
+    </svg>
+
+  </div>
+);
+
+export default BackSvgIcon;
--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import BackSvgIcon from './BackSvgIcon';
 
 const ContactPage = () => (
-  <div>
-  welcome ContactPage!
-   <Link to="/">Go home</Link>
+  <div className='page-container page-container--sunshine'>
+    <Link className='page-link--back page-link--steel-blue' to='/' title='back to menu'>
+      <BackSvgIcon />
+    </Link>
+    <div className='contact-container'>
+      <a className='contact__item' href='mailto:piotrsitarz@onet.eu'>
+        <img className='contact__image' src='./images/email.svg' alt='email adress' title='adres email' />
+        <p className='contact__paragraph'>piotrsitarz@onet.eu</p>
+      </a>
+      <a className='contact__item' href='tel:+48535428942'>
+        <img className='contact__image' src='./images/phone.svg' alt='phone number' title='numer telefonu' />
+        <p className='contact__paragraph contact__paragraph--margin'>+48 535 428 942</p>
+      </a>
+      <a className='contact__item' href='https://www.facebook.com/profile.php?id=100017226726327'>
+        <img className='contact__image' src='./images/facebook.svg' alt='facebook account' title='konto facebook'/>
+        <p className='contact__paragraph contact__paragraph--margin'>facebook</p>
+      </a>
+    </div>
   </div>
 );
 
